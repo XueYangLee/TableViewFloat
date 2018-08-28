5117c2279cdb92c921d84e3933bcc51b534eecd8
Singularity_Lee <496736912@qq.com>
2018-08-28 11:37:28 +0800

    Initial Commit

---
+++ TableViewFloat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TableViewFloat
+//
+//  Created by 李雪阳 on 2018/8/28.
+//  Copyright © 2018年 singularity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TableViewFloat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TableViewFloat
+//
+//  Created by 李雪阳 on 2018/8/28.
+//  Copyright © 2018年 singularity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
